8af1c7e42e47224e1f5ec54637797e0025b1108d
Johnson <johnson.nganga@student.moringaschool.com>
2022-08-28 03:02:13 +0300

    completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((JSON) => console.log(JSON));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +17,9 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function initialize(){
+  fetchbooks()
+}
+
+initialize()
